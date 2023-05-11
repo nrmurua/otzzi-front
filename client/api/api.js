821b7537aec5d7cfc08edcbb2fd656840659769d
nrmurua <nicolasmi.emp@gmail.com>
2023-05-11 03:52:30 -0400

    Login Signup front

--- client/api/api.js
+++ client/api/api.js
@@ -1,10 +1,30 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL}),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000"}),
     reducerPath: "adminApi",
-    tagTypes: [],
+    tagTypes: ["User"],
     endpoints: (build) => ({
-
+        register: build.query({
+            query: ({ firstName, lastName, email, password, phone, address, isCustomer, isArtist, customerId, artistId }) => ({
+                url: "/authRoutes/register",
+                method: "POST",
+                params: { firstName, lastName, email, password, phone, address, isCustomer, isArtist, customerId, artistId },
+            }),
+            providesTags: ["User"]
+        }),
+        login: build.query({
+            query: ({email, password}) => ({
+                url: "/authRoutes/register",
+                method: "GET",
+                params: {email, password},
+            }),
+            providesTags: ["User"]
+        }),
     }),
 }); 
+
+export const {
+    useRegisterQuery,
+    useLoginQuery,
+} = api;
--- client/pages/tatuador/index.jsx
+++ client/pages/tatuador/index.jsx
@@ -1,3 +1,6 @@
+import { Footer, Navbar } from '../../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../../sections';
+
 
 const TatuadorPage = () => {
   return(
